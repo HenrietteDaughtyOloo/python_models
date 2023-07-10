905187a19379dbb778281b84d71d12577189c8b3
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-07-10 20:38:28 +0300

    changes to the relationships in the order model

--- orders/models.py
+++ orders/models.py
@@ -12,9 +12,9 @@ class Orders(models.Model):
     order_date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=8,decimal_places=2)
     order_item = models.JSONField()
-    cart_relations = models.ForeignKey(MyCart, null=True, on_delete=models.CASCADE)
+    cart_relations = models.OneToOneField(MyCart, null=True, on_delete=models.CASCADE)
     customer_relations = models.ForeignKey(Customer, null=True,on_delete=models.CASCADE)
-    delivery_relations = models.ForeignKey(Delivery, null=True,on_delete=models.CASCADE )
+    delivery_relations = models.OneToOneField(Delivery, null=True,on_delete=models.CASCADE )
 
 def isExist(self):
     if Orders.objects.filter(order_item =self.order_item):
